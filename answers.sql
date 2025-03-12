22c3cdecb6fcccef8b1319722f557884a56778a7
Zamzam100 <kassimzamirah@gmail.com>
2025-03-11 19:51:55 -0700

    Added SQL work

---
+++ answers.sql
@@ -0,0 +1,5 @@
+-- Create a new database called salesDB (only if it doesn’t already exist)
+CREATE DATABASE IF NOT EXISTS salesDB;
+
+-- Drop (delete) the database called demo if it exists
+DROP DATABASE IF EXISTS demo;
